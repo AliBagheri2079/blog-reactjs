05e959f7496ae926621fc09ac119f3a431072380
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-04-27 11:48:17 +0330

    :sparkles: feat: create nasa background particle

---
+++ src/components/Particles/NasaParticle.jsx
@@ -0,0 +1,49 @@
+import { useCallback } from "react";
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+
+import nasaParticleData from "./config/nasaParticleData";
+
+/*
+  NOTE: Particles.js
+  * A lightweight JavaScript library for creating particles.
+  * In pure js project you can use this library
+  
+  NOTE: TsParticles-Engine or TsParticles
+  * A lightweight TypeScript library for creating particles. Dependency free (*), browser ready and compatible with React.js, Vue.js (2.x and 3.x), Angular, Svelte, jQuery, Preact, Inferno, Riot.js, Solid.js, and Web Components
+  * In pure js or ts project you can use this library
+   
+  NOTE: React-Particles
+  * in React and Next project, insteadof using top libraries,
+  * you can use this library
+*/
+
+const NasaParticle = () => {
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    /*
+      * you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    
+      * this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+     
+      * starting from v2 you can add only the features you need reducing the bundle size
+     */
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <>
+      <Particles
+        id="nasa-tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={nasaParticleData}
+      />
+    </>
+  );
+};
+export default NasaParticle;
---
+++ src/components/Particles/config/nasaParticleData.js
@@ -0,0 +1,105 @@
+export default {
+  particles: {
+    color: {
+      value: "#ff98a2",
+    },
+    links: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 0.5,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600,
+      },
+    },
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 1100,
+      },
+    },
+    opacity: {
+      value: 0.961765865195113,
+      random: true,
+      anim: {
+        enable: false,
+        opacity_min: 0,
+        sync: false,
+      },
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#fff2f3",
+      },
+      polygon: {
+        nb_sides: 3,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    size: {
+      value: 2.5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 4,
+        size_min: 0.3,
+        sync: false,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 100,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 0,
+        size: 0,
+        duration: 0,
+        opacity: 0.27925606185122814,
+        speed: 3,
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  detectRetina: true,
+};
