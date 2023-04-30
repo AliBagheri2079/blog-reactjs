d9e3406cd942884cea420eedc3e32e07de072615
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-05-01 00:22:30 +0330

    :art: refactor: improve root app structure and move to home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { NasaParticle } from "@/components/Particles";
+import Header from "@/layouts/public/Header";
+
+const Home = () => {
+  return (
+    <>
+      <NasaParticle />
+      <Header />
+    </>
+  );
+};
+
+export default Home;
